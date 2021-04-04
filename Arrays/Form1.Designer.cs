f5b8204059bf4cededb325c2751432da5b2b046e
Tyler Ingram <tylerphx@gmail.com>
2021-04-04 00:23:37 -0500

    Initial UI and coding

---
+++ Arrays/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Arrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.getGradesButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SeqNum";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(86, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(181, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SC#1";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(245, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SC#2";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(309, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SC#3";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(373, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "SC#4";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(428, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 23);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "SC#5";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(514, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 34);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Mean Grade:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(572, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 34);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Lowest Grade:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(636, 22);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 36);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Highest Grade";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(700, 22);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(58, 37);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Letter Grade:";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // getGradesButton
+            // 
+            this.getGradesButton.Location = new System.Drawing.Point(140, 345);
+            this.getGradesButton.Name = "getGradesButton";
+            this.getGradesButton.Size = new System.Drawing.Size(72, 35);
+            this.getGradesButton.TabIndex = 11;
+            this.getGradesButton.Text = "Get Grades";
+            this.getGradesButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(322, 345);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(72, 35);
+            this.clearButton.TabIndex = 12;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(492, 345);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(72, 35);
+            this.exitButton.TabIndex = 13;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(746, 264);
+            this.listBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 392);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.getGradesButton);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button getGradesButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ Arrays/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays
+{
+
+    public partial class Form1 : Form
+    {
+        StreamReader inputFile;
+        private String inRecord, fName, lName;
+        private double grade1, grade2, grade3, grade4, meanGrade, lowestGrade, highestGrade;
+        private string[] fileInputArray = new string[9];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Hello");
+
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+                    ReadInput();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void OpenFile()
+        {
+            InitializeComponent();
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+                    ReadInput();
+                    MessageBox.Show("Hello");
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine();
+                throw;
+            }
+        }
+
+        private void ReadInput()
+        {
+            try
+            {
+                for (int i = 0; i < fileInputArray.Length; i++)
+                {
+                    inRecord = inputFile.ReadLine();
+                    fileInputArray = inRecord.Split(',');
+                    fileInputArray[i] = inRecord;
+                    MessageBox.Show(fileInputArray[i]);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+    }
+    
+}
